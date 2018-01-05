a1dc7ea63d9ed7b77ec553ca4733760eb5386853
k3h1i013 <kmk013@naver.com>
2018-01-06 03:41:36 +0900

    2018.01.05

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    private void LateUpdate()
+    {
+        InputTouch();
+    }
+
+    void InputTouch()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.GetAxis("Mouse X") < 0)
+                transform.Translate(1f, 0, 0);
+
+            if (Input.GetAxis("Mouse X") > 0)
+                transform.Translate(-1f, 0, 0);
+
+            if (Input.GetAxis("Mouse Y") < 0)
+                transform.Translate(0, 1f, 0);
+
+            if (Input.GetAxis("Mouse Y") > 0)
+                transform.Translate(0, -1f, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/MapCreate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreate : MonoBehaviour {
+
+    public GameObject island, sea;
+
+
+    private void Awake()
+    {
+        StartCoroutine(CreateWould());
+    }
+
+    IEnumerator CreateWould()
+    {
+        int x = 32, y = 32; //맵 타일 x,y 값
+        float width = 1f, height = 1f; //블럭의 가로 세로
+
+
+        Vector3 loc = new Vector3(0, 0, 0);
+        //처음 블럭 위치를 조정
+
+        for(int i = 0; i < x; i++)
+        {
+           
+            loc.x += width;
+
+            loc.z = 0;
+            for (int j = 0; j < y; j++)
+            {
+                loc.z += height;
+                Instantiate(island, loc, Quaternion.identity);
+
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+
+    }
+}
